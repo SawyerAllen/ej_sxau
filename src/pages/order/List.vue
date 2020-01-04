4b2968f45b05202e72414f0e160ababbe252db73
aka123224 <653824194@qq.com>
2020-01-04 11:49:33 +0800

    订单管理

--- src/pages/order/List.vue
+++ src/pages/order/List.vue
@@ -1,14 +1,124 @@
 <template>
    <div>
-      订单管理
+       <el-tabs v-model="activeName" @tab-click="handleClick">
+    <el-tab-pane label="所有订单" name="first">
+       <el-table :data="orders" >
+             <el-table-column prop="id" label="订单编号"></el-table-column>
+             <el-table-column prop="orderTime" label="下单时间"></el-table-column>
+             <el-table-column prop="total" label="总价"></el-table-column>
+             <el-table-column prop="status" label="状态"></el-table-column>
+             <el-table-column prop="customerId" label="顾客id"></el-table-column>
+             <el-table-column  label="操作">
+                 <template v-slot="slot">
+                        <a  href="" @click.prevent="functionname(slot.row.id)">详情</a>
+                   </template>
+             </el-table-column>
+       </el-table>
+   </el-tab-pane>
+    <el-tab-pane label="待支付" name="second">
+       <el-table :data="orders" >
+             <el-table-column prop="id" label="订单编号"></el-table-column>
+             <el-table-column prop="orderTime" label="下单时间"></el-table-column>
+             <el-table-column prop="total" label="总价"></el-table-column>
+             <el-table-column prop="status" label="状态"></el-table-column>
+             <el-table-column prop="customerId" label="顾客id"></el-table-column>
+      </el-table>
+    </el-tab-pane>
+    <el-tab-pane label="待派送" name="third">
+       <el-table :data="orders" >
+             <el-table-column prop="id" label="订单编号"></el-table-column>
+             <el-table-column prop="orderTime" label="下单时间"></el-table-column>
+             <el-table-column prop="total" label="总价"></el-table-column>
+             <el-table-column prop="status" label="状态"></el-table-column>
+             <el-table-column prop="customerId" label="顾客id"></el-table-column>
+             <el-table-column  label="操作">
+                 <template v-slot="slot">
+                        <a  href="" @click.prevent="functionname(slot.row.id)">详情</a>
+                   </template>
+             </el-table-column>
+      </el-table>
+    </el-tab-pane>
+   <el-tab-pane label="待接单" name="fourth">
+      <el-table :data="orders" >
+             <el-table-column prop="id" label="订单编号"></el-table-column>
+             <el-table-column prop="orderTime" label="下单时间"></el-table-column>
+             <el-table-column prop="total" label="总价"></el-table-column>
+             <el-table-column prop="status" label="状态"></el-table-column>
+             <el-table-column prop="customerId" label="顾客id"></el-table-column>
+             <el-table-column  label="操作">
+                 <template v-slot="slot">
+                        <a  href="" @click.prevent="functionname(slot.row.id)">详情</a>
+                   </template>
+             </el-table-column>
+      </el-table>
+   </el-tab-pane>
+   <el-tab-pane label="待服务" name="fivth">
+       <el-table :data="orders" >
+             <el-table-column prop="id" label="订单编号"></el-table-column>
+             <el-table-column prop="orderTime" label="下单时间"></el-table-column>
+             <el-table-column prop="total" label="总价"></el-table-column>
+             <el-table-column prop="status" label="状态"></el-table-column>
+             <el-table-column prop="customerId" label="顾客id"></el-table-column>
+             <el-table-column prop="waiterId" label="员工id"></el-table-column>
+      </el-table>
+   </el-tab-pane>
+   <el-tab-pane label="待确认" name="sixth">
+      <el-table :data="orders" >
+             <el-table-column prop="id" label="订单编号"></el-table-column>
+             <el-table-column prop="orderTime" label="下单时间"></el-table-column>
+             <el-table-column prop="total" label="总价"></el-table-column>
+             <el-table-column prop="status" label="状态"></el-table-column>
+             <el-table-column prop="customerId" label="顾客id"></el-table-column>
+             <el-table-column prop="waiterId" label="员工id"></el-table-column>
+      </el-table>
+   </el-tab-pane>
+   <el-tab-pane label="已完成" name="seventh">
+      <el-table :data="orders" >
+             <el-table-column prop="id" label="订单编号"></el-table-column>
+             <el-table-column prop="orderTime" label="下单时间"></el-table-column>
+             <el-table-column prop="total" label="总价"></el-table-column>
+             <el-table-column prop="status" label="状态"></el-table-column>
+             <el-table-column prop="customerId" label="顾客id"></el-table-column>
+             <el-table-column prop="waiterId" label="员工id"></el-table-column>
+      </el-table>
+   </el-tab-pane>
+  </el-tabs>
    </div>
 </template>
 
 <script>
+import request from '@/utils/request'
+import querystring from 'querystring' 
 export default {
+    data(){
+        return{
+            orders:[],
+            
+        }
+    },
+    created(){
+      //在页面加载出来的时候加载数据
+      this.loadData();
+    },
+   methods:{
     
+      loadData(){
+        let url ="http://localhost:6677/order/findAll"
+      request.get(url).then((response)=>{
+        //将查询结果设置到customer中,this指向外部函数的this
+         this.orders = response.data;
+      })
+      },
+
+
+      
+      
+             
+
+    }
 }
 </script>
+
 
 <style scoped>
 
